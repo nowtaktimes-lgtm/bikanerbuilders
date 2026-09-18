dff98e5ebd092166b463c5f98b5afafb9c040e67
anas <anuskhan78612@gmail.com>
2026-09-19 00:16:49 +0530

    chore: Remove unused Image import from cost-estimator page

--- src/app/cost-estimator/page.tsx
+++ src/app/cost-estimator/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Metadata } from 'next';
 import CostCalculator from '@/components/CostCalculator';
-import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Construction Cost Calculator in Bikaner | Free Estimate',
